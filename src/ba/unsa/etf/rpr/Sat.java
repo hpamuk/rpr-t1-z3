1331b06b16e8ec0c191265e94aa4e7fc44425773
Hena Pamuk <hpamuk1@etf.unsa.ba>
2021-10-25 01:24:30 +0200

    Tutorijal 1 - Zadatak 3

---
+++ src/ba/unsa/etf/rpr/Sat.java
@@ -0,0 +1,36 @@
+package ba.unsa.etf.rpr;
+
+
+public class Sat {
+    int sati, minute, sekunde;
+
+    public Sat(int sati, int minute, int sekunde) { Postavi(sati, minute, sekunde); }
+        public void Postavi(int sati, int minute, int sekunde){
+            this.sati = sati;
+            this.minute = minute;
+            this.sekunde = sekunde;
+        }
+    public void sljedeci(){
+        sekunde++;
+        if(sekunde == 60) { sekunde=0; minute=0; }
+        if(minute == 60) { minute = 0; sati++; }
+        if(sati == 24) { sati = 0;}
+    }
+    public void prethodni(){
+        sekunde--;
+        if(sekunde == -1) { sekunde= 59; minute--; }
+        if(minute == -1) { minute = 59; sati--; }
+        if(sati == -1) { sati = 23;}
+    }
+    public void pomjeriZa(int pomak) {
+        if(pomak > 0) for(int i=0; i<pomak; i++) sljedeci();
+        else for(int i=0; i<-pomak; i++) prethodni();
+    }
+    public int getSekunde() { return sekunde; }
+    public int getSati() { return sati; }
+
+    public int getMinute() {
+        return minute;
+    }
+    public void ispisi() { System.out.println(sati +":" + minute + ":" + sekunde + "\n"); }
+}
